6db2151cf4a26ad6c8a74b5dbd7a30d71db03aa9
elyasaf-1 <eliasafgbi@gmail.com>
2022-03-08 18:32:51 +0200

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-const port = 3000;
+const port = 5000;
+
+mongoose.connect('mongodb://localhost:27017/kipa')
 
 app.get('/',function(req,res) {
     res.json({user:'elyasaf'});
