6d9938c2705f5d0eb84d8060ac7e39e184706dcb
elyasaf-1 <eliasafgbi@gmail.com>
2022-05-10 23:37:27 +0300

    scraper from jsdom

---
+++ models/city.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const placeSchema = mongoose.Schema({
+    name: String,
+    type: String,
+    adress: String
+});
+
+const citySchema = mongoose.Schema({
+    name: String,
+    restaurants: [placeSchema],
+    general: [placeSchema]
+});
+
+const City = mongoose.model('City', citySchema);
+
+module.exports = {
+    insert: function (city) {
+        const cityDoc = new City(city);
+        cityDoc.save();
+    },
+
+    getCity: function (cityName) {
+        return City.findOne({ name: cityName });
+    }
+};
+
